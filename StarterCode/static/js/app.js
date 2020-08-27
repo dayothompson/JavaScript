47ac94009b09ea56acab1894b69863aec426a153
dayothompson <dayo.thompson@yahoo.com>
2020-08-27 09:59:34 -0400

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,23 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+data.forEach(item => {
+    console.log(item)
+})
+
+// // Select table body
+// var body = d3.select("tbody")
+//
+// // Append row "tr"
+// var row = body.append("tr")
+
+// Append new "td"
+
+
+data.forEach(item => {
+    var row = d3.select("tbody").append("tr")
+        Object.entries(item).forEach(([key, value]) => {
+        row.append("td").text(value)
+    })
+}
+)
